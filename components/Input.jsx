2d2c212cd0a6c1bba1f248ba8e4aa468aab9f00b
cikezmedia <chikezievictorc@gmail.com>
2022-09-12 11:09:02 +0100

    Chat page updated

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BsFillEmojiLaughingFill } from 'react-icons/bs';
 import { RiSendPlane2Fill } from 'react-icons/ri';
+import { CgAttachment } from 'react-icons/cg';
 
 const Input = () => {
   return (
     <>
-      <div className='-mt-1 mr-0.5'>
+      <div className=' mr-0.5'>
         <div className='hidden lg:block relative bg-lightCard p-1 pl-2 pr-2 rounded-br-lg -top-0'>
           <form className='flex flex-row items-center space-x-3'>
-            <BsFillEmojiLaughingFill className='text-mainYellow w-7 h-7' />
+            <CgAttachment className='text-white w-7 h-7' />
             <input
               className='w-full outline-none pr-4 p-2 rounded-full pl-3 bg-gray-100 text-gray-600'
               placeholder='message...'
@@ -23,7 +24,7 @@ const Input = () => {
       <div className='absolute lg:hidden'>
         <div className='fixed right-0 bg-lightCard p-2 w-full bottom-0'>
           <form className='flex flex-row items-center space-x-3'>
-            <BsFillEmojiLaughingFill className='text-mainYellow w-7 h-7' />
+            <CgAttachment className='text-white w-7 h-7' />
             <input
               className='w-full outline-none pr-4 p-2 rounded-full pl-3 bg-gray-100 text-gray-600'
               placeholder='message...'
